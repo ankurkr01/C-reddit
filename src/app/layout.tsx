87ceabbefb834f96b060085b9109078fc986c257
Ankur Kumar Singh <iankurkumarsingh@gmail.com>
2023-09-05 01:11:25 +0530

    prisma add

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/styles/globals.css'
+
+export const metadata = {
+  title: 'C-readit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}
